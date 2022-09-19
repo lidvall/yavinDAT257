f5d7727da4f02122c148e2e740fd51d285d56644
Lukas W <75093274+PieFreak@users.noreply.github.com>
2022-09-19 10:44:52 +0200

    Add Municipality class

---
+++ src/main/java/App/Municipality.java
@@ -0,0 +1,70 @@
+import java.util.Comparator;
+import java.util.HashMap;
+
+/**
+ * This class is used to store information about a municipality (Swedish: Kommun)
+ * @author Lukas Wigren
+ * @version 1.0
+ * @since 2022-09-15
+ */
+public class Municipality {
+    private final String name;
+    private final int ID;
+    private HashMap<Integer, Double> electionParticipation;
+    
+    /**
+     * Constructor class, creates a Kommun class using its name and ID
+     * @param name  name of the municipality
+     * @param ID    ID of the municipality
+     */
+    Municipality(String name, int ID) {
+        this.name = name;
+        this.ID = ID;
+        this.electionParticipation = new HashMap<>();
+    }
+
+    /**
+     * Add function for ElectionParticipation
+     * @param year  the year
+     * @param participation the participation in %
+     */
+    public void addElectionParticipation(int year, double participation) {
+        this.electionParticipation.put(year, participation);
+    }
+
+    /**
+     * Get function for Electionparticipation
+     * @return  the HashMap of all ElectionParticipation values <Year, Participation>
+     */
+    public HashMap<Integer, Double> getElectionParticipation() {
+        return electionParticipation;
+    }
+
+    /**
+     * Get function for Electionparticipation by year
+     * @param year  the year for the participation
+     * @return  the participation for that year
+     */
+    public double getElectionParticipationByYear(int year) {
+        return electionParticipation.get(year);
+    }
+
+    /**
+     * Comparator two municipalities in case-insensitive lexicograpic order by name
+     */
+    public static final Comparator<Municipality> byNameOrder = (a,b) -> a.name.compareToIgnoreCase(b.name);
+
+    /**
+     * Comparator two municipalities in numeric order by ID
+     */
+    public static final Comparator<Municipality> byIDOrder = (a,b) -> Integer.compare(a.ID, b.ID);
+
+    /**
+     * toString function, returns the municipality as a String
+     * @return  representing String of the class
+     */
+    @Override
+    public String toString() {
+        return "Name: "+name+"\nID: "+ID;
+    }
+}
