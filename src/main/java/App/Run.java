dce4bcbf04de2ee0296d6f10ac0e747f262c41fd
PieFreak <lukas.wigren@outlook.com>
2022-10-05 00:28:48 +0200

    .

--- src/main/java/App/Run.java
+++ src/main/java/App/Run.java
@@ -3,13 +3,6 @@ package App;
 
 public class Run {
     public static void main(String[] args) throws Exception {
-        ParticipationMunicipality pm = new ParticipationMunicipality();
-        //System.out.println(pm.getMuni());
-        //(Municipality muni : pm.getMuni()){
-        //    System.out.println("Hej" + muni.getElectionParticipationByYear(2018));
-        //}
-
-        //System.out.println(pm);
         GUI gui = new GUI();
         gui.view();
     }
